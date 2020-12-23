0c4caf6a35a5f41c075efef2fb03f80c66cf3d34
HardlyCodeMan <graham@planbit.solutions>
2020-12-23 10:13:52 +0800

    Add: Contract details and connection Add: inputData function

--- dapp-template/main.js
+++ dapp-template/main.js
@@ -2,8 +2,30 @@ var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
 
 $(document).ready(function() {
-    window.ethereum.enable().then(function(accounts){
-        contractInstance = new web3.eth.Contract(abi, "0x3BdA6bd0f3DD975df54336833510d3d64f1D1638", {from: accounts[0]});
+    window.ethereum.enable().then(function(accounts) {
+        contractInstance = new web3.eth.Contract(abi, "0x46852b33798EDecCE021aba7131d93E5A8c64A72", {from: accounts[0]});
+        console.log(contractInstance);
     });
-
+    $("#add_data_button").click(inputData);
 });
+
+function inputData() {
+    // Form inputs
+    var name = $("#name_input").val();
+    var age = $("#age_input").val();
+    var height = $("#height_input").val();
+    
+    // Metamask signing params
+    var sendConfig = {
+        //value: web3.utils.toWei("1", "ether")
+        value: 1e18
+    };
+
+    console.log("name: " + name);
+    console.log("age: " + age);
+    console.log("height: " + height);
+    console.log("config: " + JSON.stringify(sendConfig));
+
+    // Let Metamask send the transaction
+    contractInstance.methods.createPerson(name, age, height).send(sendConfig);
+}
